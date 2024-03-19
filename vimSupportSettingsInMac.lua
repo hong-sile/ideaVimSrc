e76b738dc0f33c88c396517a10daad4ecb476274
hong-sile <hyukjoon.dev@gmail.com>
2024-03-19 15:58:01 +0900

    일반 상황에서도 hjkl 매핑

---
+++ vimSupportSettingsInMac.lua
@@ -0,0 +1,48 @@
+escape_keyevent = hs.eventtap.new (
+  {hs.eventtap.event.types.keyDown},
+  function (event)
+    local flags = event:getFlags()
+    local keycode = hs.keycodes.map[event:getKeyCode()]
+
+    if (keycode == 'escape') then
+      -- print("This is escape")
+      local input_korean = "com.apple.inputmethod.Korean.2SetKorean"
+      local input_english = "com.apple.keylayout.UnicodeHexInput"
+
+      local input_source = hs.keycodes.currentSourceID()
+
+      if (input_source ~= input_english) then
+        hs.keycodes.currentSourceID(input_english)
+      end
+    end
+  end
+)
+escape_keyevent:start()
+
+
+function stroke_arrow(arrow_key)
+  local event = require("hs.eventtap").event
+  event.newKeyEvent({}, arrow_key, true):post()
+  event.newKeyEvent({}, arrow_key, false):post()
+end
+
+hs.hotkey.bind({"command"}, "H",
+  function () stroke_arrow('left') end,
+  function () end,
+  function () stroke_arrow('left') end
+)
+hs.hotkey.bind({"command"}, "J",
+  function () stroke_arrow('down') end,
+  function () end,
+  function () stroke_arrow('down') end
+)
+hs.hotkey.bind({"command"}, "K",
+  function () stroke_arrow('up') end,
+  function () end,
+  function () stroke_arrow('up') end
+)
+hs.hotkey.bind({"command"}, "L",
+  function () stroke_arrow('right') end,
+  function () end,
+  function () stroke_arrow('right') end
+)
